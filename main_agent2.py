f3c3d7db309524e8933cb574c69763c72b73c8ae
srikanth-maganti <147676593+srikanth-maganti@users.noreply.github.com>
2025-04-06 21:54:17 +0530

    Update main_agent2.py

--- main_agent2.py
+++ main_agent2.py
@@ -1,7 +1,10 @@
+__import__("pysqlite3")
+import sys
+sys.modules["sqlite3"]=sys.modules.pop("pysqlite3")
+
 import streamlit as st
-# import ollama
 import os
-os.environ["STREAMLIT_WATCHER_TYPE"] = "none"
+
 
 from agents.summerization_agent import summarizer
 from agents.action_extraction_agent import action_extracter
