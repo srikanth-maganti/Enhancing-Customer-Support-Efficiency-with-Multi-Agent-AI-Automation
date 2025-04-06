2d7df34f568915f92b30e1d66c2a9b9fbe4a51a3
srikanth-maganti <147676593+srikanth-maganti@users.noreply.github.com>
2025-04-06 22:49:18 +0530

    Update gen_model.py

--- agents/gen_model.py
+++ agents/gen_model.py
@@ -1,7 +1,7 @@
 import google.generativeai as genai
 
 # Configure your API key
-genai.configure(api_key=[GEMINI_API])  # or use environment variable
+genai.configure(api_key=os.environ["GEMINI_API_TOKEN"])  # or use environment variable
 
 # Function to generate a response
 def run(prompt):
